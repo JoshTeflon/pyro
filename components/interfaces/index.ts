aa633b9518dd70ed37e203898c3045cd48de4520
JoshTeflon <jenikele21h@gmail.com>
2025-03-15 09:34:47 +0100

    move footer tsx from component to view

--- components/interfaces/index.ts
+++ components/interfaces/index.ts
@@ -1,5 +1,4 @@
 export { default as Header } from './header';
-export { default as Footer } from './footer';
 export { default as Menu } from './menu';
 export { default as SocialPlatforms } from './social-platforms';
 export { default as Button } from './button';
--- components/views/footer.tsx
+++ components/views/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { SocialPlatforms } from '.';
+import { SocialPlatforms } from '@/components/interfaces';
 
-const Footer: React.FC = () => {
+const Footer = () => {
   return (
     <footer className='py-2 mt-auto shrink-0'>
       <SocialPlatforms />
