2b2b937756136080ce42b3ebc7e757ae465f1ad2
JoshTeflon <jenikele21h@gmail.com>
2026-01-26 06:10:38 +0100

    ii6pyro/refactor: make Page component a server component again

---
+++ components/views/landing-client.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Loading from '@/app/[locale]/loading';
+
+// import { CustomCursor } from '@/components/shared';
+import { Header } from '@/components/interfaces';
+import { Connect, Gallery, Home, Music } from '@/components/views';
+
+import { useAppReady } from '@/hooks';
+import AppContextProvider from '@/contexts/AppContext';
+
+const LandingClient = () => {
+  const { isLoading, progress } = useAppReady();
+  
+  if (isLoading) return <Loading progress={progress} />;
+
+  return (
+    <AppContextProvider ready={!isLoading}>
+      {/* <CustomCursor /> */}
+      <Header  />
+      <main className='relative w-full h-full flex flex-col grow font-geist-mono tracking-wide overflow-hidden'>
+        <Home />
+        <Music />
+        <Gallery />
+        <Connect />
+      </main>
+    </AppContextProvider>
+  );
+};
+
+export default LandingClient;
