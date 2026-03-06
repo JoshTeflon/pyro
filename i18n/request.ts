54dc4c3e99551ba8dab59582717331dbb23b8956
JoshTeflon <jenikele21h@gmail.com>
2026-03-06 15:28:51 +0100

    preview/feat: update requestLocale usage on route request file

--- i18n/request.ts
+++ i18n/request.ts
@@ -4,7 +4,7 @@ import { hasLocale } from 'next-intl';
 
 import { routing } from './routing';
  
-export default getRequestConfig(async (requestLocale) => {
+export default getRequestConfig(async ({requestLocale}) => {
   const store = await cookies();
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested) ? requested : store.get('NEXT_LOCALE')?.value ?? routing.defaultLocale;
